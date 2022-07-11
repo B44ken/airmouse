e99382b9baa068354e814d3260170dd4a7f29f54
b44ken <bradboratto@gmail.com>
2022-07-11 01:19:26 -0300

    banish v1 to legacy branch

--- run.sh
+++
@@ -1,5 +0,0 @@
-SITE='vps.p27.ca'
-export CERT="/etc/letsencrypt/live/$SITE/fullchain.pem"
-export KEY="/etc/letsencrypt/live/$SITE/privkey.pem"
-
-sudo flask run --host 0.0.0.0 --cert $CERT --key $KEY --port 443
